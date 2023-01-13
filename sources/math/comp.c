c73f392f327117a58862dfe95fd6b8f99e83eaf1
Pedro Paulo <pe.pn03@gmail.com>
2023-01-13 03:07:49 -0300

    FIx comp

--- sources/math/comp.c
+++ sources/math/comp.c
@@ -6,13 +6,13 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 11:04:32 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/01/05 14:34:41 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/01/13 02:59:26 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static double	abs_double(int a)
+static double	abs_double(double a)
 {
 	if (a < 0)
 		return (-a);
@@ -24,7 +24,7 @@ t_bool	comp(double a, double b)
 	double	diff;
 
 	diff = abs_double(a) - abs_double(b);
-	if (abs_double(diff) < EPSILON)
+	if (abs_double(diff) <= EPSILON)
 		return (true);
 	return (false);
 }
