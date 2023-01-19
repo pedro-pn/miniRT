c50cdbb6414c900d125b39cae8659d1d4b3153a6
Pedro Paulo <pe.pn03@gmail.com>
2023-01-19 12:34:24 -0300

    Reorganize functions

--- sources/world/light.c
+++ sources/world/light.c
@@ -6,13 +6,17 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:43:23 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/01/19 10:47:47 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/01/19 12:34:01 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_light	*light(void)
+void	point_light(t_p3d position, t_c3d color)
 {
-	return (&data()->light);
+	t_light	*_light;
+
+	_light = light();
+	_light->position = position;
+	_light->color = color;
 }
