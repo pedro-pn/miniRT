ccf05b48e858a37f9cb1000a5e5476ab47fe617a
Pedro Paulo <pe.pn03@gmail.com>
2023-04-11 00:51:54 -0300

    Update pattern to consider groups

--- sources/patterns/pattern.c
+++ sources/patterns/pattern.c
@@ -6,7 +6,7 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:18:53 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/04/04 20:47:45 by pedro            ###   ########.fr       */
+/*   Updated: 2023/04/11 00:49:48 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ t_c3d	pattern_at(t_object obj, t_p3d point)
 	t_p3d		obj_point;
 	t_p3d		patt_point;
 
-	mx_inverse(obj.transform, &mx_inv);
-	obj_point = mx_tuple_product(mx_inv, point);
+	obj_point = world_to_object(obj, point);
 	mx_inverse(obj.pattern.transform, &mx_inv);
 	patt_point = mx_tuple_product(mx_inv, obj_point);
 	return (obj.pattern.patt_at(obj.pattern, patt_point));
