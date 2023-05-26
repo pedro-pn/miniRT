06a46da4d6e4517746338842d53a293594f81591
Pedro Paulo <pe.pn03@gmail.com>
2023-05-26 12:02:16 -0300

    Refactoring of partition_children function

--- sources/optimization/group_partition_bhv.c
+++ sources/optimization/group_partition_bhv.c
@@ -6,11 +6,20 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 23:37:26 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/05/26 11:38:01 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/05/26 12:02:21 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+static t_list	*split_children(t_group *g, t_object *obj, t_list **bucket,
+								t_list *node)
+{
+	ft_lstadd_back(bucket, ft_lstnew(obj));
+	node = node->next;
+	ft_lstremove(&g->group, obj);
+	return (node);
+}
 
 t_bucks	partition_children(t_group *g)
 {
@@ -26,20 +35,11 @@ t_bucks	partition_children(t_group *g)
 	{
 		obj = node->content;
 		if (box_contains_box(sbox.left, parent_space_bounds_of(*obj)))
-		{
-			ft_lstadd_back(&bucket.left, ft_lstnew(obj));
-			node = node->next;
-			ft_lstremove(&g->group, obj);
-			continue ;
-		}
+			node = split_children(g, obj, &bucket.left, node);
 		else if (box_contains_box(sbox.right, parent_space_bounds_of(*obj)))
-		{
-			ft_lstadd_back(&bucket.right, ft_lstnew(obj));
+			node = split_children(g, obj, &bucket.right, node);
+		else
 			node = node->next;
-			ft_lstremove(&g->group, obj);
-			continue ;
-		}
-		node = node->next;
 	}
 	return (bucket);
 }
