269b5cbe7b7f6b560a4ab3a0b0b4bc57fea7b0c8
Pedro Paulo <pe.pn03@gmail.com>
2023-06-21 15:31:40 -0300

    Add smooth_teapot demo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 18:24:18 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/06/20 00:42:07 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/06/21 15:31:00 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ void	run(void)
 	// hexagon_demo();
 	// group_test();
 	// bvh_test();
-	teapot();
+	// teapot();
+	smooth_teapot();
 }
 
 int	main(int argc, char *argv[])
