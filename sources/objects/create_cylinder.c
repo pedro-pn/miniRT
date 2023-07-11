d676cd30fa94cdf7975a85704291960f6731550f
Pedro Paulo <pe.pn03@gmail.com>
2023-07-11 11:38:49 -0300

    Change scale's denominator to double type

--- sources/objects/create_cylinder.c
+++ sources/objects/create_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 12:23:51 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/07/07 12:26:19 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/07/11 09:54:28 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	set_cylinder(t_object *cylinder, t_create_cylinder p)
 	cylinder->material.color = rgb_to_tcolor(p.color);
 	translate_object(cylinder, p.origin);
 	fast_rotate_object(cylinder, p.normal);
-	scale_object(cylinder, vector(p.diameter / 2, 1, p.diameter / 2));
+	scale_object(cylinder, vector(p.diameter / 2.0, 1, p.diameter / 2.0));
 }
 
 void	create_cylinder(t_create_cylinder params)
